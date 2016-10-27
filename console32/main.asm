b2e0af5c8eac9b39f7c7c76db9527a975350be27
Michael Dorst <code@mikefdorst.com>
2016-10-27 16:29:30 -0700

    Lab6

--- console32/main.asm
+++ console32/main.asm
@@ -1,55 +1,77 @@
-; general comments
-; This version is compatible with Visual Studio 2012 and Visual C++ Express Edition 2012
-
-; preprocessor directives
 .586
-.MODEL FLAT
+.MODEL FLAT	; only 32 bit addresses, no segment:offset
 
-; external files to link with
+INCLUDE io.h   ; header file for input/output
 
-; stack configuration
-.STACK 4096
+.STACK 4096	   ; allocate 4096 bytes for the stack
 
-; named memory allocation and initialization
 .DATA
 	
-	; We will be using DWORDS because the formula gives us final results ranging:
-	; -2000d is the value of 2(-999 + 000 - 1) + 0000, which is the minimum possible value for this problem.
-	; 11995d is the value of 2(-000 + 999 - 1) + 9999, which is the maximum possible value for this problem.
-	; [-2000d, 11995d], which exceeds the range of 16 bit signed integers ([-32768d, 32767d]),
-	; but does not exceed the range of 32 bit integers ([-2147483648d, 2147483647d]).
+	; We are using DWORD sizes, because the problem spec says to.
+	; We are using unsing unsigned because the problem says the two numbers are positive
 	
-	areaCode			DWORD	916		; 16 bit decimal
-	phoneNumberFirst3	DWORD	555		; 16 bit decimal
-	phoneNumberLast4	DWORD	1234	; 16 bit decimal
+	; These prompts will be displayed when asking for numbers
+	; They are null terminated because the input macro expects a C-string
+	number1Prompt BYTE "Please input a positive integer", 0
+	number2Prompt BYTE "Please input another positive integer", 0
 
-; procedure code
+	; inputString will be used to store the user's inputs in ASCII.
+	; It is 11 characters long because the longest input will be "4294967295" (11 characters) and the input
+	; macro appends a trailing null (11)
+	; we tried this with 10 bytes, but because of the trailing null the final character of a 10 character number would get cut off,
+	; leaving only 9 characters to represent the number.
+	inputString BYTE 11 DUP ("X")
+
+	; gcdString will be used with dtoa, which expects 11 characters + Null terminator
+	gcdString BYTE 11 DUP ("X"), 0	; String to store gcd in decimal for output
+	
+	; gcdOutputLabel will be used to label the gcd when it is displayed
+	gcdOutputLabel BYTE "Greatest Common Divisor"
+
+
 .CODE
-main	PROC
+_MainProc PROC
 	
-	; The purpose of this program is to compute the value of the expression
-	; 2 ( - zipCode + phoneNumberFirst3 - 1 ) + phoneNumberLast4
-	; where zipCode = 95822d, phoneNumberFirst3 = 555d,
-	; phoneNumberLast4 = 1234d
-	; We will be computing this value in the EAX register, because it is 16 bits wide, which is necessary
-	; for this application as discussed above.
+	; gcd := number1
+	; remainder := number2
+	; loopStart
+	;   dividend := gcd
+	;   gcd := remainder
+	;   remainder := dividend mod gcd
+	; until(remainder = 0)
 
-	mov		eax, areaCode			; EAX := areaCode
-	
-	neg		eax						; EAX := -areaCode
-	
-	add		eax, phoneNumberFirst3	; EAX := -areaCode + phoneNumberFirst3
-	
-	sub		eax, 1					; EAX := -areaCode + phoneNumberFirst3 - 1
-	
-	add		eax, eax				; EAX := 2(-areaCode + phoneNumberFirst3 - 1)
-									; actually done by adding (-areaCode + phoneNumberFirst3 - 1) to itself
-	
-	add		eax, phoneNumberLast4	; EAX := 2(-areaCode + phoneNumberFirst3 - 1) + phoneNumberLast4
+	; We are using DWORD sizes, because the problem spec says to.
+	; ebx = gcd
+	; ecx = remainder
+	; eax = dividend
 
-	mov		eax, 0					; return 0
+	input number1Prompt, inputString, 11	; get input from user (maximum 11 characters, as explained above at inputString)
+	atod inputString						; convert input from ASCII coded decimal to binary integer (stored in EAX)
+	mov ebx, eax							; gcd := user input
+
+	input number2Prompt, inputString, 11	; get input from user
+	atod inputString						; convert input from string to binary integer (stored in EAX)
+	mov ecx, eax							; remainder := user input
+
+loopStart:
+	mov eax, ebx							; dividend := gcd
+	mov ebx, ecx							; gcd := remainder
+	
+	mov edx, 0								; EDX will be used as the higher order bits for the dividend
+											; we must set EDX to 0 before dividing because the higher order bits are not significant
+	div ebx									; divide dividend by gcd (to get dividend mod gcd)
+	mov ecx, edx							; remainder := dividend mod gcd
+	cmp ecx, 0								; remainder = 0?
+	jnz loopStart							; if (remainder != 0) goto loopStart
+loopEnd:
+	
+	dtoa gcdString, ebx						; convert gcd to string (for output)
+	output gcdOutputLabel, gcdString		; output gcd
+
+
+	mov eax, 0								; exit with return code 0
+	
 	ret
+_MainProc ENDP
 
-main	ENDP
-
-END
+END   ; end of source code
